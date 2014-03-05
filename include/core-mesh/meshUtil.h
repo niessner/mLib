512e023b05e803d6a36939cc56f120da7addd6cf
msavva <msavva@stanford.edu>
2014-03-05 23:15:02 +0000

    Single color pointcloudtemplate function

--- include/core-mesh/meshUtil.h
+++ include/core-mesh/meshUtil.h
@@ -8,6 +8,8 @@ namespace MeshUtil
     TriMesh createPointCloudTemplate(const TriMesh& templateMesh, const Vector<vec3f>& points);
     TriMesh createPointCloudTemplate(const TriMesh& templateMesh, const Vector<vec3f>& points,
                                      const Vector<vec4f>& colors);
+    TriMesh createPointCloudTemplate(const TriMesh& templateMesh, const std::vector<vec3f>& points,
+                                     const vec4f& color);  // Same color for all points
 }
 
 #endif  // INCLUDE_CORE_MESH_MESHUTIL_H_
--- src/core-mesh/meshUtil.cpp
+++ src/core-mesh/meshUtil.cpp
@@ -62,3 +62,35 @@ TriMesh MeshUtil::createPointCloudTemplate(const TriMesh& templateMesh,
 
     return TriMesh(vertices, indices);
 }
+
+TriMesh MeshUtil::createPointCloudTemplate(const TriMesh& templateMesh,
+                                           const std::vector<vec3f>& points,
+                                           const vec4f& color)
+{
+    const UINT64 pointCount = points.size();
+    const UINT64 tVertices = templateMesh.vertices().size();
+    const UINT64 tIndices = templateMesh.indices().size();
+
+    Vector<TriMesh::TriMeshVertex> vertices(pointCount * tVertices);
+    Vector<UINT> indices(pointCount * tIndices);
+
+    for (UINT pointIndex = 0; pointIndex < points.size(); pointIndex++)
+    {
+        const vec3f& p = points[pointIndex];
+        const UINT64 baseVertexIndex = pointIndex * tVertices;
+
+        for (UINT vertexIndex = 0; vertexIndex < tVertices; vertexIndex++)
+        {
+            TriMesh::TriMeshVertex& v = vertices[baseVertexIndex + vertexIndex];
+            v = templateMesh.vertices()[vertexIndex];
+            v.position += p;
+            v.attributeA = color;
+        }
+        for (UINT indexIndex = 0; indexIndex < tIndices; indexIndex++)
+        {
+            indices[pointIndex * tIndices + indexIndex] = templateMesh.indices()[indexIndex] + (pointIndex * (UINT)tVertices);
+        }
+    }
+
+    return TriMesh(vertices, indices);
+}
