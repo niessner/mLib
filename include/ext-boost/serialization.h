783cba6ec8dc26ee99fd9fc8526f7b174d442b98
msavva <msavva@stanford.edu>
2014-03-12 20:24:22 +0000

    Add ext-boost (only serialization function definitions for now)

---
+++ include/ext-boost/serialization.h
@@ -0,0 +1,36 @@
+#ifndef EXT_BOOST_SERIALIZATION_H_
+#define EXT_BOOST_SERIALIZATION_H_
+
+#include <boost/serialization/utility.hpp>
+#include <boost/serialization/list.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/array.hpp>
+
+#include <boost/archive/tmpdir.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+
+#include <boost/asio/streambuf.hpp>
+#include <boost/iostreams/filtering_streambuf.hpp>
+#include <boost/iostreams/filter/zlib.hpp>
+#include <boost/iostreams/filter/bzip2.hpp>
+
+namespace boost {
+namespace serialization {
+
+template<class Archive, class T>
+inline void serialize(Archive& ar, ml::point3d<T>& p, const unsigned int version) {
+	ar & p.array;
+}
+
+template<class Archive, class T>
+inline void serialize(Archive& ar, ml::Matrix4x4<T>& m, const unsigned int version) {
+	ar & m.matrix;
+}
+
+}  // namespace boost
+}  // namespace serialization
+
+#endif  // EXT_BOOST_SERIALIZATION_H_
---
+++ include/mLibBoost.h
@@ -0,0 +1,16 @@
+#ifndef MLIBBOOST_H_
+#define MLIBBOOST_H_
+
+//
+// external globally required boost headers
+//
+#include <boost/assign.hpp>
+
+// TODO: Find appropriate set of convenience headers to include here
+
+//
+// ext-boost headers
+//
+#include "ext-boost/serialization.h"
+
+#endif  // MLIBBOOST_H_
