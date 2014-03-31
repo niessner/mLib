aea098cc5187644abd437660b6cdbd355a594bbc
msavva <msavva@stanford.edu>
2014-03-31 01:45:00 +0000

    Add define guard for _WIN32_WINNT macro in mLibBoost.h

--- include/mLibBoost.h
+++ include/mLibBoost.h
@@ -2,7 +2,9 @@
 #define MLIBBOOST_H_
 
 // Get rid of warning due to ambiguity in pre-XP vs XP and later Windows versions
+#ifndef _WIN32_WINNT
 #define _WIN32_WINNT 0x0501
+#endif
 
 //
 // external boost headers
