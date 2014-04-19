69220443278f6bb16c77621182b8c8550752130d
msavva <msavva@stanford.edu>
2014-04-19 21:49:09 +0000

    Remove redundant OpenMesh OM_STATIC_BUILD define

--- include/mLibOpenMesh.h
+++ include/mLibOpenMesh.h
@@ -3,12 +3,12 @@
 #ifndef INCLUDE_MLIBOPENMESH_H_
 #define INCLUDE_MLIBOPENMESH_H_
 
-// If you link statically against OpenMesh, you have to add
-// the define OM_STATIC_BUILD to your application. This will
-// ensure that readers and writers get initialized correctly.
-#ifndef OM_STATIC_BUILD
-#define OM_STATIC_BUILD
-#endif
+//// If you link statically against OpenMesh, you have to add
+//// the define OM_STATIC_BUILD to your application. This will
+//// ensure that readers and writers get initialized correctly.
+//#ifndef OM_STATIC_BUILD
+//#define OM_STATIC_BUILD
+//#endif
 
 #ifndef _USE_MATH_DEFINES
 #define _USE_MATH_DEFINES
