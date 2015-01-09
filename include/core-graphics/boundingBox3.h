1f835585ccc41a45c7d8d6bf3f24d4c632cb1f90
msavva <msavva@stanford.edu>
2015-01-09 04:14:23 +0000

    Add boost serialization function to BoundingBox3

--- include/core-graphics/boundingBox3.h
+++ include/core-graphics/boundingBox3.h
@@ -470,6 +470,14 @@ public:
 	}
 
 protected:
+
+  // boost archive serialization functions
+  friend class boost::serialization::access;
+  template <class Archive>
+  inline void serialize(Archive& ar, const unsigned int verion) {
+    ar & boost::serialization::make_array(parameters, 6);
+  }
+
 	union {
 		struct {
 			point3d<FloatType> minB;
