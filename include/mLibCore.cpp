ee33138d9f31dee9cc81a7b483ff24f05a98ae0c
niessner <niessner@cs.stanford.edu>
2015-02-24 03:42:15 +0000

    windows util

--- include/mLibCore.cpp
+++ include/mLibCore.cpp
@@ -28,6 +28,7 @@
 // core-util source files
 //
 #include "../src/core-util/utility.cpp"
+#include "../src/core-util/windowsUtil.cpp"
 #include "../src/core-util/directory.cpp"
 #include "../src/core-util/timer.cpp"
 #include "../src/core-util/pipe.cpp"
