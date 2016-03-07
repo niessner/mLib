357a4cac3fc4e1dc726fe241b1cdf7643fd7bcee
niessner <niessner@cs.stanford.edu>
2016-03-07 06:25:35 +0000

    added Directory::getFilesRecursive(<path>) added Directory::getDirectoriesRecursive(<path>)

--- include/core-util/utility.h
+++ include/core-util/utility.h
@@ -110,7 +110,7 @@ namespace math
 
 	template<class T>
 	inline T abs(T x) {
-		//TODO complate check that it's not an unsigned variable type
+		//TODO compile check that it's not an unsigned variable type
 		if (x < 0) { return -x; }
 		return x;
 	}
