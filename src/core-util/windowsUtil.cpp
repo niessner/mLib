335c710318c4cc68ecdab4cadb2b4c8a187cfccc
angeladai <angelaqdai@gmail.com>
2016-12-21 16:42:45 -0800

    windows get error string

--- src/core-util/windowsUtil.cpp
+++ src/core-util/windowsUtil.cpp
@@ -8,7 +8,25 @@
 namespace ml {
 
 	namespace util {
-		inline void errorExit(const std::string& functionName)
+		std::string getLastErrorString()
+		{
+			//Get the error message, if any.
+			DWORD errorMessageID = ::GetLastError();
+			if (errorMessageID == 0)
+				return std::string(); //No error message has been recorded
+
+			LPSTR messageBuffer = nullptr;
+			size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+				NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+
+			std::string message(messageBuffer, size);
+
+			//Free the buffer.
+			LocalFree(messageBuffer);
+
+			return message;
+		}
+		void errorExit(const std::string& functionName)
 		{
 			// Retrieve the system error message for the last-error code
 
